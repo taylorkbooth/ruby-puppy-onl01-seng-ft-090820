2161cc47599943c3a6a72a043bec008535693803
Taylor Booth <taylorkbooth@gmail.com>
2020-09-20 16:56:05 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,29 @@
-# Add your code here
+class Dog 
+
+    @@all = []
+
+    attr_accessor :name
+
+    def initialize(name)
+      @name = name 
+      save
+    end 
+
+    def self.all
+        @@all
+    end
+
+    def self.clear_all
+        @@all.clear
+    end 
+
+    def self.print_all
+        puts @@all.collect { |dog| dog.name }
+    end 
+
+    def save 
+        @@all << self
+    end 
+
+
+end
