30d2723329364b098ce05c7e24c3222fe61bea57
Taylor Booth <taylorkbooth@gmail.com>
2020-09-20 16:08:10 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,25 @@
-# Add your code here
+class Dog
+  
+  @@all = []
+  
+  attr_accessor :name
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+end  
+  
+  def clear_all
+  
+    
+  end
+  
+  
+  
+  
+  
+end
